1dd50ee0eda93f9ade3d0c4974f9e30238dff847
Misha Golovach <golovach.miha01@gmail.com>
2023-08-17 10:29:32 +0300

    design

--- resources/views/blocks/carousel.blade.php
+++ resources/views/blocks/carousel.blade.php
@@ -12,17 +12,14 @@
                     </video>
                 </div>
             @endforeach
-
-        </div>
-        <div class="carousel-controls">
-            <button class="carousel-control-prev" type="button" data-bs-target="#video-carousel" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#video-carousel" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
+                <button class="carousel-control-prev" type="button" data-bs-target="#video-carousel" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#video-carousel" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
         </div>
     </div>
 </section>
